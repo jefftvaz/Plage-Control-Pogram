ad35359d929497937f0fc7ee1724e4fa11f2f8de
jefftvaz <142238760+jefftvaz@users.noreply.github.com>
2024-03-22 18:35:49 -0300

    Add files via upload
    This is a first prototype, more feature is coming soon.
    
    Include:
    
    - repository
    - plage entite
    - login/password
    - menu

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Org.BouncyCastle.Bcpg;
+using PlageControl;
+using PlageControl.Login;
+using System.Runtime.InteropServices.JavaScript;
+
+
+Login user = new Login();
+if( user.Creation() == false)
+{
+    if (user.Entrance() == true)
+    {
+        
+            while (true)
+            {
+                Run run = new Run();
+                run.RunProgram();
+            }
+        Console.ReadLine();
+    }
+} 
---
+++ Run.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using PlageControl.IU;
+using PlageControl.Repository;
+using PlageControl.Services;
+
+
+namespace PlageControl
+{
+#pragma warning disable SA1600 // ElementsMustBeDocumented
+
+    public class Run
+    {
+        PlageRepository repository = new PlageRepository();
+
+        public void RunProgram()
+        {
+            Console.WriteLine("\n");
+            Interface.ShowMenuOptions();
+
+            switch (Console.ReadLine())
+            {
+                case "r":
+                    {
+                        Console.WriteLine("\n");
+                        Interface.ShowList(repository.GetAll());
+                        break;
+                    }
+                case "st":
+                    {
+                        Console.WriteLine("\n");
+                        Interface.ShowListOnTable(repository.GetAll());
+                        break;
+                    }
+                case "c":
+                    {
+                        Console.WriteLine("\n");
+                        CreatePlage.Plage();
+                        break;
+                    }
+                case "d":
+                    {
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Digite o id a ser deletado.");
+                        int.TryParse(Console.ReadLine(), out int id);
+                        repository.Delete(id);
+                        break;
+                    }
+                case "u":
+                    {
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Digite o id a ser modificado.");
+                        int.TryParse(Console.ReadLine(), out int id);
+                        repository.Update(repository.GetByID(id));
+                        break;
+                    }
+                default:
+                    {
+                        break;
+                    }
+            }
+
+
+
+        }
+
+        public void CreatDataBaseOfPlage()
+        {
+
+        }
+
+    
+    }
+}
